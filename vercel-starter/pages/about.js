4cb64f81cfa7fe0c0b40d8bd3820599204998f39
Brett <CurvyAura@gmail.com>
2025-10-11 17:29:42 -0600

    restart

--- vercel-starter/pages/about.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-export default function About(){
-  const router = useRouter()
-  useEffect(()=>{ router.replace('/sends') }, [])
-  return null
-}
